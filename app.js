c6cc7d12c258063c0f038d16572909713ce312f0
Giant Mak <giantmaktszhin1234@gmail.com>
2026-04-29 23:56:30 -0700

    added server opening

---
+++ app.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// session
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.get('/', (req, res) => {
+    res.send('Main Page');
+})
+
+// 404 handler
+app.use((req, res) => {
+    res.status(404).send('Page not found - 404');
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const app = require('./app');
 require('dotenv').config();
 
 // Import models
@@ -6,7 +7,14 @@ const User = require('./models/users');
 const Location = require('./models/locations');
 const Post = require('./models/posts');
 
+const PORT = process.env.PORT || 3000;
+
 // Connect to Atlas
 mongoose.connect(process.env.MONGODB_URI)
-    .then(() => console.log('Connected to RouteRelief database'))
+    .then(() => {
+        console.log('Connected to RouteRelief database')
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`); // Opening the server
+        });
+    })
     .catch((err) => console.log('Connection error', err));

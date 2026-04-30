43557ed7298c1a91b5aaed28e81f76451b878475
Jor123kna <jorjaknaus@icloud.com>
2026-04-30 12:15:04 -0700

    Added password encryption to signup and login js files, waiting to test when pages are done

--- login.js
+++ login.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
 
 const users = require("./users");
 
@@ -17,14 +18,18 @@ router.get("/", (req, res) => {
   `);
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const { username, password } = req.body;
 
-  const foundUser = users.find(
-    user => user.username === username && user.password === password
-  );
+  const foundUser = users.find(user => user.username === username);
 
   if (!foundUser) {
+    return res.send("Invalid username or password. <a href='/login'>Try again</a>");
+  }
+
+  const passwordMatches = await bcrypt.compare(password, foundUser.password);
+
+  if (!passwordMatches) {
     return res.send("Invalid username or password. <a href='/login'>Try again</a>");
   }
 
